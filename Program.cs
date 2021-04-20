368160b09985fae2e8ce18bb57bfad06b0993e73
singek101 <michaelmusediku@gmail.com>
2021-04-20 11:26:05 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace control_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
